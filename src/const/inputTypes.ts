d9e52bfb5b79e220a44a10ef95a6eb88a7c2e7ef
Jose Quesada <josesk8mqc@gmail.com>
2023-10-25 17:40:19 -0400

    add inputTypes.

---
+++ src/const/inputTypes.ts
@@ -0,0 +1,4 @@
+export const INPUT_STANDARD: string = "INPUT_STANDARD";
+export const SELECT_INPUT: string = "SELECT_INPUT";
+export const TEXT_FIELD_INPUT: string = "TEXT_FIELD_INPUT";
+export const PASSWORD_INPUT: string = "PASSWORD_INPUT";
